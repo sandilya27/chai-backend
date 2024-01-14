89f78a3c509b35bcf7a4eb788427a1858674cd03
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-01-14 12:42:00 +0530

    all stup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,12 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//import routes
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/user", userRouter);
+
+
 export default app;
